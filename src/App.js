8542077783d7a91fa418ec11415a7c76150ea7f4
Oktay esen <oktayesenn@gmail.com>
2023-01-18 13:50:04 -0500

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import Navbar from "./scenes/Navbar";
+import React, { useState } from 'react';
+import useMediaQuery from "./hooks/useMediaQuery";
+
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumSceens = useMediaQuery('(min-width: 1060px)');
+
+
   return (
-    <div className="App">
+    <div className="App bg-deep-blue">
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+        />
       <h1>My First React App</h1>
     </div>
   );
